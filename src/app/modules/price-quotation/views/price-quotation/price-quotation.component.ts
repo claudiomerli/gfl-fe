a3f5ba2a60044d03ac9003dd29d67c0b06559b47
giacintotucciarone@gmail.com <Giacinto1>
2021-10-30 00:29:16 +0200

    finish price quotation

--- src/app/modules/price-quotation/views/price-quotation/price-quotation.component.ts
+++ src/app/modules/price-quotation/views/price-quotation/price-quotation.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {NewspaperService} from "../../../shared/services/newspaper.service";
 import {Newspaper} from "../../../shared/model/newspaper";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {jsPDF} from "jspdf";
+import autoTable from 'jspdf-autotable'
 
 @Component({
   selector: 'app-price-quotation',
@@ -25,17 +27,20 @@ export class PriceQuotationComponent implements OnInit {
     nameNewspaper: new FormControl(''),
     costEach: new FormControl(''),
     costSell: new FormControl(''),
-    numberOfEditors: new FormControl(''),
+    numberOfEditors: new FormControl('',Validators.required),
     expense: new FormControl(''),
-    earn: new FormControl(''),
     revenue: new FormControl(''),
+    earn: new FormControl(''),
   })
 
+  priceQuotationTableTemp: any = [];
 
   ngOnInit(): void {
   }
 
   loadNewspaper(event: any) {
+    this.newspaper = undefined
+    this.priceQuotationTemp.reset();
     if (!["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", "Enter"].includes(event.key)) {
       this.newspaperService
         .find(event.target.value)
@@ -64,10 +69,45 @@ export class PriceQuotationComponent implements OnInit {
   }
 
   onSubmit() {
+
+    if(this.priceQuotationTemp.invalid){
+      return
+    }
+
     this.priceQuotationTemp.patchValue({
       expense: this.priceQuotationTemp.value.numberOfEditors * this.priceQuotationTemp.value.costEach,
       revenue: this.priceQuotationTemp.value.numberOfEditors * this.priceQuotationTemp.value.costSell,
+    })
+    this.priceQuotationTemp.patchValue({
       earn: this.priceQuotationTemp.value.revenue - this.priceQuotationTemp.value.expense,
     })
+
+
+    this.priceQuotationTableTemp.push({
+      nameNewspaper: this.priceQuotationTemp.value.nameNewspaper,
+      costEach: this.priceQuotationTemp.value.costEach,
+      costSell: this.priceQuotationTemp.value.costSell,
+      numberOfEditors: this.priceQuotationTemp.value.numberOfEditors,
+      expense: this.priceQuotationTemp.value.expense,
+      revenue: this.priceQuotationTemp.value.revenue,
+      earn: this.priceQuotationTemp.value.earn,
+    });
+
+  }
+
+  deleteElement(priceQuotation: any) {
+
+    const index = this.priceQuotationTableTemp.indexOf(priceQuotation);
+    if (index > -1) {
+      this.priceQuotationTableTemp.splice(index, 1);
+    }
+  }
+
+  generateAndDownloadPdf() {
+
+    let nameFile = "Preventivo" + new Date().toLocaleDateString();
+    const doc = new jsPDF()
+    autoTable(doc, {html: '#my-table'})
+    doc.save(nameFile + "pdf")
   }
 }
