78328fed136989222b348838c19ca415b2d2ecf0
Gianmarco Bonelli <g.bonelli@solvingteam.it>
2021-12-03 12:02:58 +0100

    introdotti messaggi di errore e validazione form utente

--- src/app/modules/user/components/user-create-form/user-create-form.component.ts
+++ src/app/modules/user/components/user-create-form/user-create-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl, FormGroup, ValidatorFn, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from "@angular/forms";
 import {SaveUserDto} from "../../../shared/messages/users/save-user.dto";
 
 @Component({
@@ -9,10 +9,24 @@ import {SaveUserDto} from "../../../shared/messages/users/save-user.dto";
 })
 export class UserCreateFormComponent implements OnInit {
 
+  createUserForm: FormGroup = new FormGroup({});
+  formSubmitted: boolean = false;
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.createUserForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      fullname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mobilePhone: new FormControl(''),
+      level: new FormControl(''),
+      remuneration: new FormControl(''),
+      role: new FormControl(null, [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      repeatPassword: new FormControl('', [Validators.required, this.passwordMatchesValidatorFunction()])
+    });
   }
 
   @Input()
@@ -21,34 +35,20 @@ export class UserCreateFormComponent implements OnInit {
   @Output()
   formSubmit = new EventEmitter<SaveUserDto>();
 
-  passwordMatchesValidatorFunction: ValidatorFn = formGroup => {
-    let passwordValue = formGroup.get("password")?.value;
-    let repeatePasswordValue = formGroup.get("repeatePassword")?.value;
-
-    if ((passwordValue && repeatePasswordValue) && passwordValue != repeatePasswordValue) {
-      return {
-        "passwordMismatches": true
-      }
+  passwordMatchesValidatorFunction(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      let passwordValue = this.createUserForm.controls.password?.value;
+      let repeatPasswordValue = control.value;
+      return (passwordValue && repeatPasswordValue) && passwordValue != repeatPasswordValue
+        ? {repeatPassword: {value: control.value, messaggio: 'Le password non corrispondono'}} : null;
     }
-
-    return null;
   };
 
-  createUserForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    fullname: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
-    mobilePhone: new FormControl(''),
-    level: new FormControl(''),
-    remuneration: new FormControl(''),
-    role: new FormControl(null, [Validators.required]),
-
-    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-    repeatePassword: new FormControl('', [Validators.required, Validators.minLength(8)])
-  }, [this.passwordMatchesValidatorFunction])
-
   onSubmit() {
-    this.formSubmit.emit(this.createUserForm.value as SaveUserDto)
+    this.formSubmitted = true;
+    if(this.createUserForm.valid) {
+      this.formSubmit.emit(this.createUserForm.value as SaveUserDto);
+    }
   }
 
 }
