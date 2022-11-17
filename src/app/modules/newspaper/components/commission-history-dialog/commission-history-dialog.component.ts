c41ca0683f669df666b205617befdc68a47e5bba
Claudio Merli <cladlomerli99@gmail.com>
2022-11-17 12:25:10 +0100

    update

---
+++ src/app/modules/newspaper/components/commission-history-dialog/commission-history-dialog.component.ts
@@ -0,0 +1,24 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {ProjectCommission} from "../../../shared/model/project";
+import {projectCommissionStatus} from "../../../shared/utils/utils";
+
+@Component({
+  selector: 'app-commission-history-dialog',
+  templateUrl: './commission-history-dialog.component.html',
+  styleUrls: ['./commission-history-dialog.component.scss']
+})
+export class CommissionHistoryDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<CommissionHistoryDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ProjectCommission,
+  ) {}
+
+  projectCommissionStatus = projectCommissionStatus
+  ngOnInit(): void {
+
+  }
+
+
+}
