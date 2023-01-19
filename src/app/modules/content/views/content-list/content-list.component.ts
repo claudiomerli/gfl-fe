9c569999e49817634bc12730cfa1c73b9e8baf76
Claudio Merli <cladlomerli99@gmail.com>
2023-01-19 20:21:42 +0100

    fix

--- src/app/modules/content/views/content-list/content-list.component.ts
+++ src/app/modules/content/views/content-list/content-list.component.ts
@@ -14,6 +14,10 @@ import {debounceTime} from "rxjs/operators";
 import {ProjectService} from "../../../shared/services/project.service";
 import {NewspaperService} from "../../../shared/services/newspaper.service";
 import {isObject} from "rxjs/internal-compatibility";
+import {User} from "../../../shared/model/user";
+import {UserService} from "../../../shared/services/user.service";
+import {Store} from "@ngxs/store";
+import {AuthenticationState} from "../../../store/state/authentication-state";
 
 @Component({
   selector: 'app-content-list',
@@ -34,11 +38,15 @@ export class ContentListComponent implements OnInit {
     contentStatus: new FormControl<string | null>(null),
     project: new FormControl<Project | null | string>(null, [validateObject]),
     newspaper: new FormControl<Newspaper | null | string>(null, [validateObject]),
+    editor: new FormControl<User | null | string>(null, [validateObject]),
   });
   displayFullnameProject = (project: Project) => project?.name;
   displayFullnameNewspaper = (newspaper: Newspaper) => newspaper?.name;
+  displayFullnameEditor = (editor: User) => editor?.fullname || "";
+
   projects: Project[] = [];
   newspapers: Newspaper[] = [];
+  editors: User[] = [];
 
 
   search() {
@@ -48,6 +56,7 @@ export class ContentListComponent implements OnInit {
           contentStatus: this.contentFilter.value.contentStatus || "",
           newspaperId: (this.contentFilter.value.newspaper as Newspaper)?.id || "",
           projectId: (this.contentFilter.value.project as Project)?.id || "",
+          editorId: (this.contentFilter.value.editor as User)?.id || ""
         }, this.pagination)
         .subscribe((result) => {
           this.actualPage.next(result);
@@ -55,10 +64,17 @@ export class ContentListComponent implements OnInit {
     }
   }
 
-  constructor(private contentService: ContentService, private projectService: ProjectService, private newspaperService: NewspaperService) {
+  constructor(
+    private contentService: ContentService,
+    private projectService: ProjectService,
+    private newspaperService: NewspaperService,
+    private userService: UserService,
+    private store : Store
+  ) {
   }
 
   ngOnInit(): void {
+    let actualUser = this.store.selectSnapshot(AuthenticationState.user)!;
     this.search()
 
     this.columnsToShow.push("createdDate")
@@ -66,6 +82,9 @@ export class ContentListComponent implements OnInit {
     this.columnsToShow.push("project")
     this.columnsToShow.push("newspaper")
     this.columnsToShow.push("status")
+    if(["ADMIN","CHIEF_EDITOR"].includes(actualUser.role!)){
+      this.columnsToShow.push("editor")
+    }
     this.columnsToShow.push("actions")
 
     this.contentFilter.controls.project.valueChanges
@@ -101,6 +120,24 @@ export class ContentListComponent implements OnInit {
         } else {
           this.contentFilter.controls.newspaper.setValue(null)
           this.newspapers = []
+        }
+      })
+
+    this.contentFilter.controls.editor.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe((value) => {
+        if (value != null && typeof value === "string" && value !== "") {
+          this.userService
+            .find(value, "EDITOR", new PaginationDto(0, 50, "ASC", "fullname"))
+            .subscribe(value => {
+              this.editors = value.content
+            })
+        } else if (value != null && typeof value === "object") {
+          this.search()
+          this.editors = []
+        } else {
+          this.contentFilter.controls.editor.setValue(null)
+          this.editors = []
         }
       })
 
--- src/app/modules/shared/messages/content/find-content-filter.dto.ts
+++ src/app/modules/shared/messages/content/find-content-filter.dto.ts
@@ -2,4 +2,5 @@ export interface FindContentFilterDto {
   contentStatus?: string;
   projectId?: number | string;
   newspaperId?: number | string;
+  editorId?: number | string;
 }
